2e23e9f402e99699a7392f2c6681a78771a15c8d
baney <baneymelo@gmail.com>
2021-01-27 16:59:10 -0500

    the collection is added

---
+++ Mixed Messages/index.js
@@ -0,0 +1,22 @@
+
+const collection = [
+
+    signs = ["Leo", "Aries", "Sagittarius","Taurus","Virgo","Capricorn","Gemini","Libra","Aquarium","Cancer","Scorpio","Pisces"],
+
+    attributes = ["are generous and open-minded", "are adventurous and courageous", "are intelligent and philosophical","people are romantics","people born under the sign are very analytical","are prudent and practical",
+    "people are versatile and quick-witted","their romantic nature is coupled with serenity","have outstanding intelligent and a good sense of humor",
+"people are sentimental","are passionate individuals with a magnetic personality", "are sensitive and sympathetic"],
+    personality = ["people with a good sense of humor", "show deep love for magnificence and luxury", "they are generally independent","they tend to think overly on any given subject","their love for style and beauty is apparent in their way of living","They plan before playing any game of life",
+"they are independent thinkers with great originality in their thoughts and actions","are balanced in nature and know how to remain calm at all occasions","their love towards life is evident from their living",
+"they are cautious in their actions","they have clarity of thought and expression","they can go out of their way to help their dear ones"],
+    weaks = ["people act impatiently", "they may be egotistic and bossy", "their moods and whims can be bothersome","they may come across as sadistic individuals who are orthodox and rigid","they can be possessive about everything they have","their overcritical nature can become a cause of their troubles",
+    "they tend to worry too much on certain issues and fail in managing their stress","at times, they find it difficult to express themselves","they may appear indifferent and less emotional",
+"they are subject to varying moods","due to their possessive nature, they become jealous quite easily","opinions of others can easily influence themv"],
+
+    love = ["their impulsiveness love is incurable","good lover","the most passionate of all","often frequents shady places to find love","they are a liares by nature, they are a real heartbreaker","they doesn't care about love, in fact hates it",
+    "they are fearful in relationships, they don't trust anyone","they are true Romeo if they talk to you they make you fall in love","they despise love and even themselves","they fully engage in relationships","they are very violent and aggressive in sex","depending on their mood, they only love you, they only want you, they only hate you, or all inclusive"],
+    number = ["11","99","3","45","78","6","8","13","21","48","25","12"],
+    color = ["yellow","blue","red","gray","green","purple","white","black","pink","brown","cian","orange"]
+]
+
+console.log(collection[0][0])
